1affe42462481bcf427f3e9da691d9a785c89112
mw46594 <mw46594@zut.edu.pl>
2021-05-19 20:24:37 +0200

    Add gitignore

--- cmake-build-debug32b/UILD_ARCH=-target i686-pc-windows-gnu/CMakeFiles/Kulki.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Kulki.
-# This may be replaced when dependencies are built.
--- cmake-build-debug32b/src/CMakeFiles/source.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsource.a"
-  "libsource.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
